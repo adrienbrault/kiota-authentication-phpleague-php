efeddbd5c70b398d501c7f249b6f18022da6167f
Adrien Brault <adrien.brault@gmail.com>
2023-02-22 13:56:13 +0100

    feat: custom http client
    # Conflicts:
    #	src/PhpLeagueAccessTokenProvider.php

--- src/PhpLeagueAccessTokenProvider.php
+++ src/PhpLeagueAccessTokenProvider.php
@@ -12,13 +12,12 @@ namespace Microsoft\Kiota\Authentication;
 use Http\Promise\FulfilledPromise;
 use Http\Promise\Promise;
 use Http\Promise\RejectedPromise;
-use League\OAuth2\Client\Grant\GrantFactory;
 use League\OAuth2\Client\Provider\AbstractProvider;
 use League\OAuth2\Client\Provider\GenericProvider;
 use League\OAuth2\Client\Token\AccessToken;
 use Microsoft\Kiota\Abstractions\Authentication\AccessTokenProvider;
 use Microsoft\Kiota\Abstractions\Authentication\AllowedHostsValidator;
-use Microsoft\Kiota\Authentication\Oauth\OnBehalfOfGrant;
+use Microsoft\Kiota\Authentication\Oauth\ProviderFactory;
 use Microsoft\Kiota\Authentication\Oauth\TokenRequestContext;
 
 /**
@@ -43,6 +42,10 @@ class PhpLeagueAccessTokenProvider implements AccessTokenProvider
      */
     private array $scopes;
     /**
+     * @var array<string, object>
+     */
+    private array $collaborators;
+    /**
      * @var AccessToken|null Token object to re-use before expiry
      */
     private ?AccessToken $cachedToken = null;
@@ -57,14 +60,14 @@ class PhpLeagueAccessTokenProvider implements AccessTokenProvider
      * @param array $scopes
      * @param array $allowedHosts
      */
-    public function __construct(TokenRequestContext $tokenRequestContext, array $scopes = [], array $allowedHosts = [])
+    public function __construct(TokenRequestContext $tokenRequestContext, array $scopes = [], array $allowedHosts = [], ?GenericProvider $oauthProvider = null)
     {
         $this->tokenRequestContext = $tokenRequestContext;
         if (empty($scopes)) {
             $scopes = ['https://graph.microsoft.com/.default'];
         }
         $this->scopes = $scopes;
-        
+
         $this->allowedHostsValidator = new AllowedHostsValidator();
         if (empty($allowedHosts)) {
             $this->allowedHostsValidator->setAllowedHosts(["graph.microsoft.com", "graph.microsoft.us", "dod-graph.microsoft.us", "graph.microsoft.de", "microsoftgraph.chinacloudapi.cn", "canary.graph.microsoft.com"]);
@@ -72,7 +75,7 @@ class PhpLeagueAccessTokenProvider implements AccessTokenProvider
             $this->allowedHostsValidator->setAllowedHosts($allowedHosts);
         }
 
-        $this->initOauthProvider();
+        $this->oauthProvider = $oauthProvider ?? ProviderFactory::create($tokenRequestContext);
     }
 
     /**
@@ -122,25 +125,6 @@ class PhpLeagueAccessTokenProvider implements AccessTokenProvider
     public function getOauthProvider(): AbstractProvider
     {
         return $this->oauthProvider;
-    }
-
-    /**
-     * Initialises a PHP League provider
-     */
-    private function initOauthProvider(): void
-    {
-        $grantFactory = new GrantFactory();
-        // Add our custom grant type to the registry
-        $grantFactory->setGrant('urn:ietf:params:Oauth:grant-type:jwt-bearer', new OnBehalfOfGrant());
-
-        $this->oauthProvider = new GenericProvider([
-            'urlAccessToken' => "https://login.microsoftonline.com/{$this->tokenRequestContext->getTenantId()}/oauth2/v2.0/token",
-            'urlAuthorize' => "https://login.microsoftonline.com/{$this->tokenRequestContext->getTenantId()}/oauth2/v2.0/authorize",
-            'urlResourceOwnerDetails' => 'https://graph.microsoft.com/oidc/userinfo',
-            'accessTokenResourceOwnerId' => 'id_token'
-        ], [
-            'grantFactory' => $grantFactory
-        ]);
     }
 
 }
